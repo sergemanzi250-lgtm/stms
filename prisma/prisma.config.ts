3cffdc65ff772cf8d95349c99a340c0140cbfccc
sergemanzi250-lgtm <sergemanzi250@gmail.com>
2026-01-05 18:13:31 +0200

    Remove prisma.config.ts to fix Vercel build error

--- prisma/prisma.config.ts
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'prisma/config'
-
-export default defineConfig({
-  schema: './schema.prisma',
-  database: {
-    adapter: 'sqlite3',
-    url: 'file:./dev.db'
-  }
-})
